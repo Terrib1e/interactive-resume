bd9c7e6d49b240b2064532456d9f2bc946996925
Elijah Clark <162511917+103168@users.noreply.github.com>
2025-06-12 17:32:29 -0400

    changed to light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // src/App.tsx
-import { ThemeProvider } from '@/components/theme/ThemeProvider';
-import { ThemeToggle } from '@/components/theme/ThemeToggle';
-import { EditModeManager } from '@/components/EditModeManager';
-import { Toaster } from '@/components/ui/toaster';
-import { resumeData } from '@/data/resumeData';
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
+import { ThemeToggle } from "@/components/theme/ThemeToggle";
+import { EditModeManager } from "@/components/EditModeManager";
+import { Toaster } from "@/components/ui/toaster";
+import { resumeData } from "@/data/resumeData";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="resume-theme">
+    <ThemeProvider defaultTheme="light" storageKey="resume-theme">
       <div className="relative min-h-screen bg-background">
         <ThemeToggle />
         <EditModeManager initialData={resumeData as never} />
